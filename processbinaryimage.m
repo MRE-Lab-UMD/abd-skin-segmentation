745976ee05173e35e5688e8415125870912cdaf1
anirudhtopiwala <topiwala.anirudh@gmail.com>
2019-03-27 02:41:52 -0400

    Adding test data of pratheepan and FSD in Coco format

--- processbinaryimage.m
+++ processbinaryimage.m
@@ -2,11 +2,11 @@
 % clc;clear all;close all
 
 %% Input Directory
-dir_img = './all abdomen/images/';
-dir_mask = './all abdomen/skin_maksed_renamed/';
-img_input1 = './all abdomen/train/skin_train2019/';
+dir_img = './Input/test_pratheepan/family/skin_test2019/';
+dir_mask = './Input/test_pratheepan/family/annotations/';
+img_input1 = './Input/dataset-pratheepan/testingdata/FamilyPhoto/';
 % randfolder = './all abdomen/newtest/';
-mask_input1 = './all abdomen/train/annotations/';
+mask_input1 = './Input/dataset-pratheepan/groundtruth/FamilyPhoto/';
 img_input = imageDatastore(img_input1,'IncludeSubfolders',true,'LabelSource','foldernames');
 mask_input  = imageDatastore(mask_input1,'IncludeSubfolders',true,'LabelSource','foldernames');
 
@@ -16,11 +16,11 @@ mask_input  = imageDatastore(mask_input1,'IncludeSubfolders',true,'LabelSource',
 % mask_input  = imageDatastore(mask1,'IncludeSubfolders',true,'LabelSource','foldernames');
 
 %% Loop
-k= 769;
-l=1;
- for i=11:1:14
+k= 32;
+% l=1;
+ for i=1:1:100
 %    gtimg = imread(strcat(img_input1,int2str(i),'.jpeg'));  
-%      gtimg = imread(img_input.Files{i});  
+     gtimg = imread(img_input.Files{i});  
 %      gtimg = imread(gTruth.DataSource.Source{i});
      
 %    imshow(gtimg);
@@ -28,15 +28,15 @@ l=1;
 %      mask = imread(strcat(mask_input1,sprintf('%s%04d','',i),'.png'));
     maskimg = imread(mask_input.Files{i});
 %     maskimg = maskimg(:,:,1);
-%     maskimg(maskimg>10) =255;
-%     maskimg(maskimg<10) =0;
+    maskimg(maskimg>30) =255;
+    maskimg(maskimg<30) =0;
 %     maskimg = 255*imread(strcat(mask_input1,'Label_',int2str(i),'.png'));  
-%    imshowpair(gtimg,maskimg,'Montage');
-%    imwrite(gtimg,sprintf('%s%05d.jpeg', dir_img,i+k));
+   imshowpair(gtimg,maskimg,'Montage');
+   imwrite(gtimg,sprintf('%s%05d.jpeg', dir_img,i+k));
 %    imwrite(gtimg,sprintf('%s%05d.jpeg',randfolder,i+k));
 %     imshow(maskimg)
-%    imwrite(maskimg,strcat(dir_mask,sprintf('%s%05d','',l+k),'_skin_',sprintf('%s%05d','',l+k),'.png'));
-    l=l+1;
+   imwrite(maskimg,strcat(dir_mask,sprintf('%s%05d','',i+k),'_skin_',sprintf('%s%05d','',i+k),'.png'));
+%     l=l+1;
    % %    imwrite(currentimage,sprintf('%s0%01d.jpg', input1,i));
 %    delete(img_input.Files{i})
 %    delete(mask_input.Files{i})
