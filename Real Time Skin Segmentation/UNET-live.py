f83945b0f01f957bfb936e1adc7fdda93f779a01
Lydia Zoghbi <lalzogbi@terpmail.umd.edu>
2019-12-28 19:56:33 -0500

    Adding comments on real-time skin segmentation python code

--- Real Time Skin Segmentation/UNET-live.py
+++ Real Time Skin Segmentation/UNET-live.py
@@ -1,9 +1,4 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[9]:
-
-
+# Importing bunch of libraries
 import os
 import sys
 import time
@@ -36,8 +31,7 @@ warnings.filterwarnings('ignore', category=UserWarning, module='skimage')
 seed = 42
 random.seed = seed
 
-
-
+# Define mean IOU metric, if needed
 def mean_iou(y_true, y_pred):
    prec = []
    for t in np.arange(0.5, 1.0, 0.05):
@@ -49,45 +43,22 @@ def mean_iou(y_true, y_pred):
        prec.append(score)
    return K.mean(K.stack(prec), axis=0)
 
-
-
+# Load the trained model, you can use yours or the model we provide in our code
+# Make sure to set up the path correctly
 model = load_model('../models/UNET.h5', custom_objects={'mean_iou': mean_iou})
-# model = load_model('./model-withouvdm-abd-3Mar2019.h5', custom_objects={'mean_iou': mean_iou})
 
-
-# ## Single Image
-# cv2.namedWindow("preview")
-# cv2.namedWindow("normal")
-
-# frame= imread('./wound3.jpg')
-# # frame= cv2.imread('./128.jpg')
-# cv2.imshow("normal", frame)
-
-# img = resize(frame, (IMG_HEIGHT, IMG_WIDTH), mode='constant',preserve_range=True)
-# newframe= np.expand_dims(img, axis=0)
-# # preds = model.predict((newframe), verbose=0)    
-# preds = model.predict(newframe[:int(newframe.shape[0])], verbose=0)    
-# # preds_img = preds.astype(np.uint8)
-# preds_img = (preds > 0.5).astype(np.uint8)
-# cv2.imshow("preview", np.squeeze(preds_img*255, axis=0))
-# flag = True
-# while (flag):
-# 	key = cv2.waitKey(2)
-# 	if key == 27: # exit on ESC
-# 		flag = False
-# cv2.destroyWindow("preview")
-# cv2.destroyWindow("normal")
-
-
+# Open window to visualize the segmentation
 cv2.namedWindow("preview")
 cv2.namedWindow("normal")
 vc = cv2.VideoCapture(0)
 
-if vc.isOpened(): # try to get the first frame
+# Try to get first frame
+if vc.isOpened(): 
     rval, frame = vc.read()
 else:
     rval = False
 
+# Loop until user presses ESC key
 while rval:
     rval, frame = vc.read()
     ima1 = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
@@ -95,14 +66,13 @@ while rval:
                                       preserve_range=True)
     newframe= np.expand_dims(img, axis=0)
     preds = model.predict(newframe, verbose=0)    
-    # preds_img = preds.astype(np.uint8)
     preds_img = (preds > 0.5).astype(np.uint8)
-    # np.squeeze(preds_img*255, axis=0)
-    # print(preds_img.shape)
     cv2.imshow("preview", np.squeeze(preds_img*255, axis=0))
     cv2.imshow("normal",frame)
     key = cv2.waitKey(20)
     if key == 27: # exit on ESC
         break
+
+# Close window
 cv2.destroyWindow("preview")
 cv2.destroyWindow("normal")
